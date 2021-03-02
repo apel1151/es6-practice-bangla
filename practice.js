60a9a009f43cfded10a2521bc197a56b701a35db
apel2021 <apelmahmud1151@gmail.com>
2021-03-03 04:17:15 +0800

    updated

--- practice.js
+++ practice.js
@@ -1,68 +1,68 @@
-//////////////////// finding property value from an object which if from an array /////////////////////////////
+////////////////// finding property value from an object which if from an array /////////////////////////////
 
 
-// const students = [
+const students = [
 
 
-//     { id: 11111, name: 'hasan' },
-//     { id: 22222, name: 'asha' },
-//     { id: 33333, name: 'omok' },
-//     { id: 44444, name: 'tomok' }
-// ]
+    { id: 11111, name: 'hasan' },
+    { id: 22222, name: 'asha' },
+    { id: 33333, name: 'omok' },
+    { id: 44444, name: 'tomok' }
+]
 
 
-// function getName(array, property) {
-//     const output = [];
+function getName(array, property) {
+    const output = [];
 
-//     for (i = 0; i < array.length; i++) {
+    for (i = 0; i < array.length; i++) {
 
-//         output.push(array[i][property]);
-//     }
-//     return output;
-// }
+        output.push(array[i][property]);
+    }
+    return output;
+}
 
-// const result = getName(students, 'id');
-// console.log(result);
+const result = getName(students, 'id');
+console.log(result);
 
-//////////////////// Alternative way to get property form object which is from an array //////////////
-
-
-// const students = [
-//     { id: 345, name: 'rafi' },
-//     { id: 356, name: 'safi' },
-//     { id: 267, name: 'saki' }
-// ];
-
-// const newName = [];
-// for (i = 0; i < students.length; i++) {
-//     const element = students[i];
-
-//     const getName = element.name;
-//     newName.push(getName);
-
-// }
-
-// console.log(newName);
+////////////////// Alternative way to get property form object which is from an array //////////////
 
 
+const students = [
+    { id: 345, name: 'rafi' },
+    { id: 356, name: 'safi' },
+    { id: 267, name: 'saki' }
+];
 
-/////////////////////// map with function for an array /////////////////////////
+const newName = [];
+for (i = 0; i < students.length; i++) {
+    const element = students[i];
 
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const getName = element.name;
+    newName.push(getName);
 
+}
 
-// const result = numbers.map(function(element) {
-
-//     return element * element;
-// });
-
-// console.log(result);
-
-////////////////// Alternavite way /////////////////
-
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(newName);
 
 
 
-// const output = numbers.map(element => element * element);
-// console.log(output);
+///////////////////// map with function for an array /////////////////////////
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+const result = numbers.map(function(element) {
+
+    return element * element;
+});
+
+console.log(result);
+
+//////////////// Alternavite way /////////////////
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+
+const output = numbers.map(element => element * element);
+console.log(output);
